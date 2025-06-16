d580114288c33f889936361b80010b67fdea64ef
ykob <info@tplh.net>
2025-06-16 21:40:05 +0900

    init prisma for auth server.

---
+++ apps/auth-server/src/index.ts
@@ -0,0 +1,23 @@
+// src/index.ts
+
+import dotenv from "dotenv";
+import express from "express";
+
+// .env ファイルを読み込む
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// リクエストボディのJSONをパースするためのミドルウェア
+app.use(express.json());
+
+// サーバーが正常に動作しているか確認するためのヘルスチェック用エンドポイント
+app.get("/health", (req, res) => {
+  res.status(200).send("Auth Server is healthy!");
+});
+
+// サーバーを起動
+app.listen(PORT, () => {
+  console.log(`Auth Server is running on port ${PORT}`);
+});
