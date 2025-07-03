acd88993ce7a0f7f3c653e5bef2f56d3842784af
ykob <info@tplh.net>
2025-07-03 22:42:52 +0900

    update migrate command.

--- apps/auth-server/prisma/migrations/20250703133831_/migration.sql
+++ apps/auth-server/prisma/migrations/20250703133831_/migration.sql
@@ -1,3 +1,18 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('USER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'USER',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateTable
 CREATE TABLE "RefreshToken" (
     "id" TEXT NOT NULL,
@@ -9,6 +24,9 @@ CREATE TABLE "RefreshToken" (
 
     CONSTRAINT "RefreshToken_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "RefreshToken_hashedToken_key" ON "RefreshToken"("hashedToken");
